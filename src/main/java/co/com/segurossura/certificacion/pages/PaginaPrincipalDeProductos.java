e7f7de27ecdc435c694b8645027582ae705fd0c7
DiegoPinzon20 <diegopip62@gmail.com>
2022-07-05 14:09:46 -0500

    Se completa el mapeo de la pagina principal del catalogo de productos

---
+++ src/main/java/co/com/segurossura/certificacion/pages/PaginaPrincipalDeProductos.java
@@ -0,0 +1,56 @@
+package co.com.segurossura.certificacion.pages;
+
+import net.serenitybdd.core.pages.WebElementFacade;
+import net.thucydides.core.annotations.At;
+import net.thucydides.core.pages.PageObject;
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.FindBy;
+import java.util.List;
+
+@At("https://www.saucedemo.com/inventory.html")
+public class PaginaPrincipalDeProductos extends PageObject {
+
+    @FindBy(xpath = "//span[@class='title']")
+    private WebElementFacade tituloPaginaPrincipal;
+
+    @FindBy(xpath = "//span[@class='shopping_cart_badge']")
+    private WebElementFacade contadorDeProductosEnElCarro;
+
+    @FindBy(xpath = "//a[@class='shopping_cart_link']")
+    private WebElementFacade shoppingButton;
+
+    @FindBy(xpath="//select[@class='product_sort_container']")
+    private WebElementFacade listaDeFiltros;
+
+    @FindBy(xpath = "//button[text()='Add to cart']")
+    private List<WebElementFacade> botonesParaAgregarAlCarrito;
+
+    public boolean esVisibleElTituloDeLaPaginaPrincipal(){
+        return tituloPaginaPrincipal.isVisible();
+    }
+
+    public void agregarProductosAlCarrito(String nombreProducto){
+        String buttonLocator = String.format("//div[text()='%s']/../../../div/button", nombreProducto);
+        getDriver().findElement(By.xpath(buttonLocator)).click();
+    }
+
+    public int obtenerNumeroDeProductosEnElCarrito(){
+        return Integer.parseInt(contadorDeProductosEnElCarro.getText());
+    }
+
+    public void verProductosEnElCarrito(){
+        shoppingButton.click();
+    }
+
+    public void ordenarProductosPorMenorPrecio(){
+        listaDeFiltros.selectByValue("lohi");
+    }
+
+    public void ordenarProductosPorPrecioMayor(){
+        listaDeFiltros.selectByValue("hilo");
+    }
+
+    public void agregarElPrimerProducto(){
+        botonesParaAgregarAlCarrito.get(0).click();
+    }
+}
