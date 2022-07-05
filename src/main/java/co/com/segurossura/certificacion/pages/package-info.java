12cbf32f53a26cbedec030d75da6008c599ab472
DiegoPinzon20 <diegopip62@gmail.com>
2022-07-05 12:05:17 -0500

    Se agregan los paquetes para el desarrollo de los tests

---
+++ src/main/java/co/com/segurossura/certificacion/pages/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Este paquete contiene la estructura del arqueotipo de
+ * desarrollo de pruebas POM
+ *
+ * @author Diego Pinzon
+ * @version 1.0
+ */
+package co.com.segurossura.certificacion.models;
