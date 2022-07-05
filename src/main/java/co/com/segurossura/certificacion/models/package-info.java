c8018da7e0d5d55616a03ce69f5de12c5846f516
DiegoPinzon20 <diegopip62@gmail.com>
2022-07-05 14:44:01 -0500

    Se realiza el mapeo de objetos y funcionalidades de la pagina de finalizar proceso de compra

--- src/main/java/co/com/segurossura/certificacion/models/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * Este paquete contiene los modelos necesarios para
- * el desarrollo de la automatizacion
- *
- * @author Diego Pinzon
- * @version 1.0
- */
-package co.com.segurossura.certificacion.models;
---
+++ src/main/java/co/com/segurossura/certificacion/pages/PaginaFinalizarCompra.java
@@ -0,0 +1,62 @@
+package co.com.segurossura.certificacion.pages;
+
+import co.com.segurossura.certificacion.models.InformacionDelUsuarioModelo;
+import net.serenitybdd.core.pages.PageObject;
+import net.serenitybdd.core.pages.WebElementFacade;
+import net.thucydides.core.annotations.At;
+import org.openqa.selenium.support.FindBy;
+
+@At("https://www.saucedemo.com/checkout-step-one.html")
+public class PaginaFinalizarCompra extends PageObject {
+
+    @FindBy(id = "continue")
+    WebElementFacade btnContinuar;
+
+    @FindBy(id = "cancel")
+    WebElementFacade btnCancelar;
+
+    @FindBy(id = "first-name")
+    WebElementFacade entradaPrimerNombre;
+
+    @FindBy(id = "last-name")
+    WebElementFacade entradaDeApellido;
+
+    @FindBy(id = "postal-code")
+    WebElementFacade entradaCodigoPostal;
+
+    @FindBy(xpath = "//h3[@data-test='error']")
+    WebElementFacade mensajeDeErrorFinalizarCompra;
+
+    @FindBy(id = "finish")
+    WebElementFacade btnFinalizarCompra;
+
+    @FindBy(xpath = "//h2[text()='THANK YOU FOR YOUR ORDER']")
+    WebElementFacade mensajeDeCompraExitosa;
+
+    public void ingresarInformacionDelUsuario(InformacionDelUsuarioModelo informacionDelUsuario){
+        ingresarPrimerNombre(informacionDelUsuario.getFirstName());
+        ingresarApellido(informacionDelUsuario.getLastName());
+        ingresarCodigoPostal(informacionDelUsuario.getPostalCode());
+        finalizarProcesoDeCompra();
+    }
+    private void ingresarPrimerNombre(String primerNombre){
+        entradaPrimerNombre.type(primerNombre);
+    }
+
+    private void ingresarApellido(String apellido){
+        entradaDeApellido.type(apellido);
+    }
+
+    private void ingresarCodigoPostal(String codigoPostal){
+        entradaCodigoPostal.type(codigoPostal);
+    }
+
+    private void finalizarProcesoDeCompra(){
+        btnContinuar.click();
+        btnFinalizarCompra.click();
+    }
+
+    public boolean fueExitosaLaCompra(){
+        return mensajeDeCompraExitosa.isVisible();
+    }
+}
